a177f17d6ee6cf8de67f5f11ea50373b5fc4c261
White-Tiger-PX <191647395+White-Tiger-PX@users.noreply.github.com>
2025-02-15 05:35:48 +0300

    Добавил docstring-и в модуль set_logger

--- set_logger.py
+++ set_logger.py
@@ -1,15 +1,34 @@
+"""
+Модуль для создания логгера.
+"""
 import os
 import logging
 
 from datetime import datetime
 
 
-def set_logger(log_folder=None):
+def set_logger(log_folder: str = None) -> logging.Logger:
+    """
+    Создает и настраивает логгер для записи логов в файл и вывод в консоль.
+
+    Логгер использует формат:
+    `YYYY-MM-DD HH:MM:SS - LEVELNAME - Сообщение`
+
+    Если указана папка `log_folder`, логи также сохраняются в файле
+    с именем в формате `YYYY-MM-DD HH-MM-SS.log`.
+
+    Args:
+        log_folder (str, optional): Путь к папке для сохранения логов.
+            Если `None`, логи пишутся только в консоль.
+
+    Returns:
+        logging.Logger: Настроенный объект логгера.
+    """
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-    if log_folder:  # Создание файла с логами только если указана папка
+    if log_folder:
         log_filename = datetime.now().strftime('%Y-%m-%d %H-%M-%S.log')
         log_file_path = os.path.join(log_folder, log_filename)
 
