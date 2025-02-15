d16f27110acb8fe8e11fa8fcd01cbe9cf5e836bb
White-Tiger-PX <191647395+White-Tiger-PX@users.noreply.github.com>
2025-02-15 03:48:41 +0300

    Добавил docstring-и в модуль fetch_access_token

--- fetch_access_token.py
+++ fetch_access_token.py
@@ -1,8 +1,31 @@
+"""
+Модуль для получения учётных данных от Twitch.
+
+Краткое описание функций:
+- fetch_access_token: Получает учётные данные для Twitch.
+"""
 import time
 import requests
 
 
 def fetch_access_token(client_id, client_secret, logger):
+    """
+    Получает учётные данные для Twitch.
+
+    Функция выполняет запрос к API Twitch для получения учётных данных.
+    Функция будет повторять попытку каждые 60 секунд до успешного получения токена.
+
+    Args:
+        client_id (str): Идентификатор клиента Twitch.
+        client_secret (str): Секрет клиента Twitch.
+        logger (logging.Logger): Логгер.
+
+    Returns:
+        str: Access token для использования в API запросах.
+
+    Raises:
+        Exception: Если произошла неизвестная ошибка.
+    """
     while True:
         try:
             token_url = "https://id.twitch.tv/oauth2/token?client_id=" + \
@@ -13,7 +36,11 @@ def fetch_access_token(client_id, client_secret, logger):
             token = token_response.json()
 
             return token["access_token"]
-        except Exception as err:
-            logger.error(f"Ошибка при получении токена: {err}")
+        except requests.exceptions.RequestException as err:
+            logger.error(f"Ошибка при получении токена в модуле fetch_access_token: {err}")
 
-            time.sleep(30)
+            time.sleep(60)
+        except Exception as err:
+            logger.error(f"Неизвестная ошибка в модуле fetch_access_token: {err}")
+
+            return None
