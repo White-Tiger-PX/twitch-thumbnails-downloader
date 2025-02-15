d0edb6bf7c15f05c34e911e2f16427f7bb23eb4d
White-Tiger-PX <191647395+White-Tiger-PX@users.noreply.github.com>
2025-02-15 05:53:44 +0300

    Добавлены docsting-и и вызов исключения в модуле get_twitch_user_id

--- get_twitch_user_id.py
+++ get_twitch_user_id.py
@@ -1,3 +1,6 @@
+"""
+Модуль для получения Twitch user_id по имени пользователя через API.
+"""
 import requests
 
 import config
@@ -6,9 +9,20 @@ from set_logger import set_logger
 from fetch_access_token import fetch_access_token
 
 
-def get_twitch_user_id(user_name, headers, main_logger):
+def get_twitch_user_id(user_name: str, headers: dict, main_logger) -> str:
     """
-    Получает user_id через API Twitch.
+    Получает user_id пользователя Twitch по его имени через API.
+
+    Args:
+        user_name (str): Имя пользователя Twitch для поиска.
+        headers (dict): Заголовки для запроса, включая Client-ID и Authorization.
+        main_logger (logging.Logger): Логгер.
+
+    Returns:
+        str or None: Возвращает user_id пользователя, если найден, или None в случае ошибки.
+
+    Raises:
+        Exception: Если возникает ошибка в процессе получения данных через API или обработки данных.
     """
     try:
         logger = main_logger.getChild('get_twitch_user_id')
@@ -26,14 +40,13 @@ def get_twitch_user_id(user_name, headers, main_logger):
 
         return user_id
     except requests.exceptions.RequestException as err:
-        logger.error(f"Ошибка при получении ID для пользователя [ {correct_user_name} ]: {err}")
+        logger.error("Ошибка при получении ID для пользователя [ %s ]: %s", correct_user_name, err)
 
-        return None
+        raise
     except Exception as err:
-        logger.error(f"Ошибка при работе с базой данных для пользователя [ {correct_user_name} ]: {err}")
+        logger.error("Ошибка при работе с базой данных для пользователя [ %s ]: %s", correct_user_name, err)
 
-        return None
-
+        raise
 
 
 if __name__ == "__main__":
@@ -41,21 +54,18 @@ if __name__ == "__main__":
 
     logger = set_logger(config.log_folder)
 
-    client_id = config.client_id
-    client_secret = config.client_secret
-
     access_token = fetch_access_token(
-        client_id=client_id,
-        client_secret=client_secret,
-        logger=logger
+        client_id     = config.client_id,
+        client_secret = config.client_secret,
+        logger        = logger
     )
 
-    headers = {"Client-ID": client_id, "Authorization": f"Bearer {access_token}"}
+    headers = {"Client-ID": config.client_id, "Authorization": f"Bearer {access_token}"}
 
     user_id = get_twitch_user_id(
-        user_name=user_input,
-        headers=headers,
-        main_logger=logger
+        user_name   = user_input,
+        headers     = headers,
+        main_logger = logger
     )
 
     logger.info("User ID: %s", user_id)
